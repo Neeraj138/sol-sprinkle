81eb1d7cdd7d03617dfd5a891081038aa331d78c
Neeraj138 <neeraj.r@greyorange.com>
2024-10-04 11:58:04 +0530

    Initial setup

---
+++ src/components/WalletContextProvider.jsx
@@ -0,0 +1,19 @@
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react"
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui"
+import { clusterApiUrl } from "@solana/web3.js"
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+const WalletContextProvider = ({children}) => {
+    const endpoint = clusterApiUrl("devnet");
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={[]}>
+            <WalletModalProvider>
+            {children}
+            </WalletModalProvider>
+        </WalletProvider>
+    </ConnectionProvider>
+  )
+}
+
+export default WalletContextProvider
